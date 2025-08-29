727adb55054d163f9cfd6e72593efc370af2ef15
HoTung <hoviettung2@gmail.com>
2025-08-30 06:31:35 +0700

    add: day03Lab

---
+++ Day03Lab_231230949_29_08_2025/Day03Lab_231230949_29_08_2025/Controllers/PassDataController.cs
@@ -0,0 +1,30 @@
+﻿using Day03Lab_231230949_29_08_2025.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day03Lab_231230949_29_08_2025.Controllers
+{
+    public class PassDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewDataDemo()
+        {
+            ViewData["name"] = "Hồ Tùng";
+            ViewData["time"] = DateTime.Now;
+            var students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Nguyen Van A", Active = true, Age = 20, Gender = "Nam" },
+                new Student { Id = 2, Name = "Tran Thi B", Active = false, Age = 21, Gender = "Nữ" },
+                new Student { Id = 3, Name = "Le Van C", Active = true, Age = 19, Gender = "Nam" },
+                new Student { Id = 4, Name = "Pham Thi D", Active = true, Age = 22, Gender = "Nữ" },
+                new Student { Id = 5, Name = "Hoang Van E", Active = false, Age = 20, Gender = "Nam" }
+            };
+
+            ViewData["students"] = students;
+
+            return View();
+        }
+    }
+}
---
+++ Day03Lab_231230949_29_08_2025/Day03Lab_231230949_29_08_2025/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Day03Lab_231230949_29_08_2025.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
