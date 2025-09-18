f2423bd65590d4a6781252fc2dfc07db8f64aa96
HoTung <hoviettung2@gmail.com>
2025-09-18 22:11:46 +0700

    add: lab04

---
+++ Day04Lab_231230949_12_09_2025/Day04Lab_231230949_12_09_2025/Day04Lab_231230949_12_09_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day04Lab_231230949_12_09_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
