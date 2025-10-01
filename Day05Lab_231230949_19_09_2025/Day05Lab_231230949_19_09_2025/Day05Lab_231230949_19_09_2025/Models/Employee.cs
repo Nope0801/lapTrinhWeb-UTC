70b9820ec952f08d2177c058d7a03452b078748a
HoTung <hoviettung2@gmail.com>
2025-10-02 00:42:24 +0700

    add:lab05

---
+++ Day05Lab_231230949_19_09_2025/Day05Lab_231230949_19_09_2025/Day05Lab_231230949_19_09_2025/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day05Lab_231230949_19_09_2025.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ Day05Lab_231230949_19_09_2025/Day05Lab_231230949_19_09_2025/Day05Lab_231230949_19_09_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day05Lab_231230949_19_09_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
